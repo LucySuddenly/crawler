43e7fcfb55c991b3a349f2c994dcfc0cb0d4b430
Lucy Suddenly <suddenlylucy@gmail.com>
2019-11-01 10:05:29 -0700

    moved grid array to state

--- src/components/grid.js
+++ src/components/grid.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import Tile from './tile.js'
 
 class Grid extends Component {
+    constructor(){
+        super()
+        this.state = {
+            grid: new Array(121).fill(0)
+        }
+    }
     render() { 
         return ( 
             <div id="grid">
-                {new Array(121).fill(0).map(() => {
+                {this.state.map(() => {
                 return <Tile/>
                 })}
             </div>
